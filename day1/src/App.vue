86ee9bd8f5168590e96cc6f485e2678d218c0065
Warat Wongmaneekit <canopybanj@gmail.com>
2018-10-24 11:04:37 +0700

    Add filter

--- day1/src/App.vue
+++ day1/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <ul>
-      <li v-for="todo in todos" :key="todo.time" v-html="todo.text">
-
+      <li v-for="todo in todos" :key="todo.time">
+        {{todo.text | capitalize}}
       </li>
     </ul>
   </div>
@@ -11,21 +11,29 @@
 <script>
 export default {
   name: 'App',
+  filters: {
+    // addText (value) {
+    //   return value + ' 1'
+    // },
+    capitalize (value) {
+      return value.toUpperCase()
+    }
+  },
   data () {
     return {
       todos: [
         {
-          text: '<strong>Todo1</strong>',
+          text: 'todo1',
           time: 1540352507,
           completed: true
         },
         {
-          text: 'Todo2',
+          text: 'todo2',
           time: 1540352529,
           completed: true
         },
         {
-          text: 'Todo3',
+          text: 'todo3',
           time: 1540352535,
           completed: false
         }
