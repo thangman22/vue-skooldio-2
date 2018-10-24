3d298735f1d29baad78a5729e9c91f3740e345ed
Warat Wongmaneekit <canopybanj@gmail.com>
2018-10-24 10:30:18 +0700

    Lesson 1 Data

---
+++ day1/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>{{list.join(',')}}</h1>
+    <h1>{{number}}</h1>
+    <h1>{{text}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      text: 'Hello Skooldio',
+      number: 10,
+      list: ['a','b','c']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
